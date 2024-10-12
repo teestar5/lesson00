02f3c0578797eee11febc61e13749826a8795acf
teestar5 <147434843+teestar5@users.noreply.github.com>
2024-10-12 21:46:29 +0300

    Add files via upload

---
+++ lesson00.py
@@ -0,0 +1,12 @@
+from pickletools import float8
+
+print((9**0.5)*5)  #"1st program"
+
+print(9.99>9.98, 1000!=1000.1) #boolean  #"2nd program"
+
+print((2+2)*2)  #"3rd program"
+print((2*2)+2)
+print(((2+2)*2)==(2*2)+2)
+
+i = float('123.456')
+print(int(i*10)%10)   #"4th program"
